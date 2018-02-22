38a28834eddf934dc66f4aec64a0ad33c0b5c921
Gerard Casas Saez <casassg@gmail.com>
2018-02-22 04:08:03 -0700

    update prizes and all. :shipit:

--- assets/js/liveSchedule.js
+++ assets/js/liveSchedule.js
@@ -8,24 +8,7 @@
 var scheduleUrl = "https://docs.google.com/spreadsheets/d/1MkLve58YjYuJdRrmG8-7rpTCaa7v9mspzmMninApx5g";
 var corsUrl = "http://cors-proxy.htmldriven.com/?url=" + scheduleUrl + "/export?format=csv";
 
-liveSchedule = function() {
-	// Get data from CORS
-	var urlData = jQuery.getJSON(corsUrl);
 
-	// Wait 3 seconds to retrieve all data
-	// Then update table
-	setTimeout(function() {
-		if (urlData.responseJSON == undefined) {
-			console.log("Data not yet received.");
-			return;
-		}
-		var rawData = urlData.responseJSON.body;
-		var timetable = updateTable(rawData);
-	}, 3000);
-}
-
-// Poll for updates every 10 seconds
-setInterval(liveSchedule, 10000);
 
 updateTable = function(jsonData) {
 	var days = jsonData.split("Day 2");
@@ -33,33 +16,49 @@ updateTable = function(jsonData) {
 		var lines = days[i].split("\n")
 		var agenda = [];
 		var output = [];
-
+		var first_line = true;
 		for (var j = 0; j < lines.length; j++) {
 			
 			if (lines[j][0] == ",") {
 				continue;
 			}
 			var contents = lines[j].split(",");
+			if (first_line){
+				output.push('<thead>')
+			}
 			output.push("<tr><td>" + contents.join("</td><td>") + "</td></tr>");
-			
-			// // creates a JSON object after some parsing
-			// if (j != 0) {
-			// 	var item = {};
-			// 	item.startTime = contents[0].split(" ")[0];
-			// 	item.startMeridiem = contents[0].split(" ")[1];
-			// 	item.endTime = contents[1].split(" ")[0];
-			// 	item.endMeridiem = contents[1].split(" ")[1];
-			// 	item.event = contents[2];
-			// 	item.location = contents[3];
-			// 	agenda.push(item);
-			// }
+			if (first_line){
+				output.push('</thead><tbody>');
+				first_line = false;
+			}
+
 		}
+		output.push('</tbody>')
 
 		// update the table
 		var div = document.getElementById('scheduleTable' + i);
-		output = "<table>" + output.join("") + "</table>";
+		output = "<table class=\"highlight responsive-table\">" + output.join("") + "</table>";
+
 		div.innerHTML = output;
 	}
 
 	return agenda;
 }
+
+liveSchedule = function() {
+	// Get data from CORS
+	$.get(corsUrl, function(data) {
+		var timetable = updateTable(data.body);
+		console.log("Schedule updated!")
+	});
+}
+
+$(function() {
+	if (document.getElementById('scheduleTable0') !== null) {
+		liveSchedule();
+		setInterval(liveSchedule, 1000 * 60 * 2);
+		// Poll for updates every 2 mins
+	}
+});
+
+
