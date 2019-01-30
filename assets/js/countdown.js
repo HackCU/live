101026ecf4b001858db25162253ae84762ec198a
Gerard Casas Saez <casassg@gmail.com>
2019-01-30 16:15:28 -0700

    fix countdown and move to vuejs

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -2,76 +2,45 @@
 # Frontmatter needed to take start and end time variables from _config.yml
 ---
 
-// Countdown Timer
-
-// Calculate remaining time
-function getTimeRemaining(startTime, endTime) {
-
-	let today = new Date();
-
-	// After hackathon
-	if (Date.parse(today) > Date.parse(endTime)) {
-		return {
-		'total': 0,
-		'days': 0,
-		'hours': 0,
-		'minutes': 0,
-		'seconds': 0
-		};
-	}
-
-        let t = 0;
-
-	// Before hackathon
-	if (Date.parse(today) < Date.parse(startTime)) {
-                t = Date.parse(endTime) - Date.parse(startTime);
-        }
-
-        // During hackathon
-        else {
-                t = Date.parse(endTime) - Date.parse(today);
-        }
-
-	let seconds = Math.floor( (t/1000) % 60 );
-	let minutes = Math.floor( (t/1000/60) % 60 );
-	let hours = Math.floor( (t/(1000*60*60)) % 24 );
-	let days = Math.floor( t/(1000*60*60*24) );
-	return {
-		'total': t,
-		'days': days,
-		'hours': hours,
-		'minutes': minutes,
-		'seconds': seconds
-	};
-} // end of calculating remaining time
-
-
-// Initialize and update clock during event
-function initializeClock(id, startTime, endTime) {
-
-	let clock = document.getElementById(id);
-	let hours = clock.querySelector('#hours');
-	let minutes = clock.querySelector('#minutes');
-	let seconds = clock.querySelector('#seconds');
-
-	// Update clock every second
-	let remainingTime = setInterval(function() {
-		let time = getTimeRemaining(startTime, endTime);
-		hours.innerHTML = ('0' + time.hours).slice(-2);
-		minutes.innerHTML = ('0' + time.minutes).slice(-2);
-		seconds.innerHTML = ('0' + time.seconds).slice(-2);
+var app = new Vue({
+	el: '.countdown',
+	data: {
+	  hackingBegins:"{{ site.start_time | date_to_xmlschema }}",
+	  hackingEnds: "{{ site.end_time  | date_to_xmlschema}}",
+	  now: Math.trunc((new Date()).getTime() / 1000)
+	},
+	mounted() {
+		window.setInterval(() => {
+			this.now = Math.trunc((new Date()).getTime() / 1000);
+		},1000);
+	},
+	filters: {
+		twodigits: function(value) {
+			if (value < 0) {
+				return '00';
+			}
+			if (value.toString().length <= 1) {
+				return `0${value}`;
+			}
+			return value;
+		},
+	},
+	computed: {
+		timeleft() {
+			if (this.now < Math.trunc(Date.parse(this.hackingBegins) / 1000)){
+				return Math.trunc(Date.parse(this.hackingEnds) / 1000) - Math.trunc(Date.parse(this.hackingBegins) / 1000);
+			}
+		  	return Math.trunc(Date.parse(this.hackingEnds) / 1000) - this.now;
+		},
+		seconds() {
+		  	return Math.trunc(this.timeleft) % 60;
+		},
+		minutes() {
+		  return Math.trunc((this.timeleft) / 60) % 60;
+		},
+		hours() {
+		  return Math.trunc((this.timeleft) / 60 / 60);
+		},
 		
-		//stop updating once time remaining becomes 0
-		if (time.total <= 0) {
-		clearInterval(remainingTime);
-		}
-	}, 1000); // end of updating clock
-	
-} // end of setting up clock
-
-window.onload = function() {
-	let hackingBegins = "{{ site.start_time | date_to_xmlschema }}";
-	let hackingEnds = "{{ site.end_time  | date_to_xmlschema}}";
-	initializeClock('countdown-timer', hackingBegins, hackingEnds);
-	initializeClock('countdown-timer-mobile', hackingBegins, hackingEnds);
-};
+	}
+  })
