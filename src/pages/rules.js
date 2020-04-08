e585e3f4c991323d0aa004be910e8530ca1ba504
kpfromer <kpfromer2@gmail.com>
2020-04-07 20:52:24 -0600

    Update rules page to use rebass

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import { css } from '@emotion/core';
 import { useStaticQuery, graphql } from 'gatsby';
 import SEO from '../components/seo';
+import { Box } from 'rebass';
 
 export default () => {
   // get the rule.md from data based on the frontmatter property `path: "rules"`
@@ -16,12 +17,17 @@ export default () => {
   return (
     <Layout title="Rules">
       <SEO title="Rules" />
-      <div
-        css={css`
+      <Box
+        // TODO: Integrate with theme ui some how?
+        css={(theme) => css`
+          a {
+            text-decoration: none;
+            color: ${theme.colors.primary};
+          }
           li {
             list-style-type: square !important;
             margin-left: 1em;
-            font-weight: 300;
+            // font-weight: 300;
             line-height: 2em;
           }
         `}
