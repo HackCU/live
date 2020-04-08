a582723f460ebbf106cb3d28dd0714bcb4ffd8fd
kpfromer <kpfromer2@gmail.com>
2020-04-07 20:50:48 -0600

    Update events page to use rebass

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { Link as ReactLink, useStaticQuery, graphql } from 'gatsby';
 import { CollectionItem, Collection } from 'react-materialize';
 import { css } from '@emotion/core';
 import OutsideLink from '../components/outside-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Text, Heading, Flex, Box, Link } from 'rebass';
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -32,47 +33,61 @@ export default () => {
       }
     }
   `);
-  const workshops = data.allWorkshopsYaml.edges.map(val => val.node);
-  const sideEvents = data.allSideEventsYaml.edges.map(val => val.node);
+  const workshops = data.allWorkshopsYaml.edges.map((val) => val.node);
+  const sideEvents = data.allSideEventsYaml.edges.map((val) => val.node);
   return (
     <Layout title="Events">
-      <p>
-        Check out the <Link to="/schedule">schedule</Link> to know when all
-        these workshops and more are happening!
-      </p>
-      <h4>Workshops</h4>
-      <Collection>
-        {workshops.map(item => (
-          <CollectionItem key={item.id}>
-            <h5
-              css={css`
-                font-weight: 500;
-              `}
-            >
+      <Text>
+        Check out the{' '}
+        <Link as={ReactLink} to="/schedule">
+          schedule
+        </Link>{' '}
+        to know when all these workshops and more are happening!
+      </Text>
+      <Heading as="h4" my={3}>
+        Workshops
+      </Heading>
+      <Flex flexDirection="column">
+        {workshops.map((item) => (
+          <Box
+            key={item.id}
+            as="li"
+            p={3}
+            variant="outline"
+            sx={{
+              listStyle: 'none'
+            }}
+          >
+            <Text as="h5" fontWeight={500}>
               {item.title}
-            </h5>
-            <small>by {item.author}</small>
-            <p>{item.description}</p>
-          </CollectionItem>
+            </Text>
+            <Text as="small">by {item.author}</Text>
+            <Text>{item.description}</Text>
+          </Box>
         ))}
-      </Collection>
+      </Flex>
 
-      <h4>Side Events</h4>
-      <Collection>
-        {sideEvents.map(item => (
-          <CollectionItem key={item.id}>
-            <h5
-              css={css`
-                font-weight: 500;
-              `}
-            >
+      <Heading as="h4" my={3}>
+        Side Events
+      </Heading>
+      <Flex flexDirection="column">
+        {sideEvents.map((item) => (
+          <Box
+            key={item.id}
+            as="li"
+            p={3}
+            variant="outline"
+            sx={{
+              listStyle: 'none'
+            }}
+          >
+            <Text as="h5" fontWeight={500}>
               {item.title}
-            </h5>
-            <p>{item.description}</p>
-            
-          </CollectionItem>
+            </Text>
+            <Text>{item.description}</Text>
+          </Box>
         ))}
-      </Collection>
+      </Flex>
     </Layout>
   );
 };
